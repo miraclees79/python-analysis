b4f726fabd95c1f2420d83c0773b17e9114bf79a
miraclees79 <adam.glogowski@gmail.com>
2025-03-16 23:11:06 +0100

    Update Download-oceny-FI.py
    Added support for wait and clicks

--- Download-oceny-FI.py
+++ Download-oceny-FI.py
@@ -6,7 +6,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import os
 import io
