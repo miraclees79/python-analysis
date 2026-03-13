03d701e7f9d0bf6db568407ad421aff6d47c650b
miraclees79 <adam.glogowski@gmail.com>
2026-03-13 13:30:32 +0100

    typo

--- knf_explore/knf_stooq_match.py
+++ knf_explore/knf_stooq_match.py
@@ -300,7 +300,7 @@ def residual_name(name_without_prefix: str, brand_token: str = "") -> str:
 TFI_NO_STOOQ: set[str] = {
     "pfr tfi s.a.",
     "mtfi s.a.",
-    "velofund tfi s.a.",
+    "velofunds tfi s.a.",
 }
 
 # ---------------------------------------------------------------------------
