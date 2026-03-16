9a78a94c39f15f99ed00e77fae7e1e744b47fa42
miraclees79 <adam.glogowski@gmail.com>
2026-03-16 15:17:28 +0100

    MC

--- dev_single_asset_monitoring/strat_test_runfile_with_MC.py
+++ dev_single_asset_monitoring/strat_test_runfile_with_MC.py
@@ -102,7 +102,7 @@ chosen_mode = "full"
 VOL_WINDOW = 20
 FORCE_FILTER_MODE = ["ma","mom"]
 # options ["ma","mom"] ["ma"] ["mom"] ["fund"] None (fully auto)
-RUN_MONTE_CARLO = False # MC parameter robustness
+RUN_MONTE_CARLO = True # MC parameter robustness
 RUN_BLOCK_BOOTSTRAP = False # Run bootstrap robustness test
 TRAIN_YEARS_EQ = 8
 TEST_YEARS_EQ = 2
